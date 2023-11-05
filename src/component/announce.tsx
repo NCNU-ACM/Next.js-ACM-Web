afd8c5158eb64776b22db14e3b97b4716c841ae6
amy <jolin51120@gmail.com>
2023-11-05 20:44:47 +0800

    修改公告內容

--- src/component/announce.tsx
+++ src/component/announce.tsx
@@ -21,7 +21,7 @@ interface Props {}
 const announce = [
   {
     people: "Web master",
-    say: "我有幫新加入的成員在我們的ACM server (acm1.ipv6.club.tw) 建立帳號了！連線方式：帳號：s學號@163.22.20.115 -p 2021密碼：學號這台server是專屬我們ACM會員才可以使用的～",
+    say: "我有幫新加入的成員在我們的ACM server (acm1.ipv6.club.tw) 建立帳號了！這台server是專屬我們ACM會員才可以使用的！",
   },
   {
     people: "Web master",
@@ -49,7 +49,7 @@ const announce = [
   },
   {
     people:"Server小組的陳郁芳",
-    say:"已經將新加入的成員在我們的ACM2 (acm2.ipv6.club.tw) 建立帳號~（要連VPN）連線方式如下：帳號：s學號@163.22.2.2 -p 2023密碼：學號"
+    say:"已經將新加入的成員在我們的ACM2 (acm2.ipv6.club.tw) 建立帳號~"
   },
 ];
 export function Announce(Props: Props) {
